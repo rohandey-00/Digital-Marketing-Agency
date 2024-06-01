ed84fc5f2d9ccbd9c97ce4ee4f9ff642fa785efe
Rohan dey <deyrohan608@gmail.com>
2024-06-01 12:53:10 +0530

    build front page with contact up pop up

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.getElementById("contuct").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "flex";
 });
+
+document.querySelector(".closebtn").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
