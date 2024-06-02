72142ccb1843bab97d07621d9899b47d330581ba
rohan dey <deyrohan608@gmail.com>
2024-06-02 09:48:01 +0530

    complect 2nd page

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ document.getElementById("contuct").addEventListener("click", function(){
 document.querySelector(".closebtn").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 });
+
+
+// function for link opening in new tab
+
+btns = document.querySelectorAll('#readmorbtn');
+
+    for (btn of btns){
+        btn.addEventListener("click", ()=>{
+            const url='https://www.fylehq.com/';
+            window.open(url);
+        });
+    }
+
