75d1bf291334578f46d4d4b276e148b08df577d7
rohan dey <deyrohan608@gmail.com>
2024-06-02 19:45:42 +0530

    Project complete.

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ btns = document.querySelectorAll('#readmorbtn');
 
 
 document.querySelector(".distwo").addEventListener("click", () => {
-    document.querySelector(".thirdLeftPic img").src = "/assets/image.png"; 
+    document.querySelector(".thirdLeftPic img").src = "./assets/image.png"; 
 });
 
 document.querySelector(".disone").addEventListener("click", () => {
-    document.querySelector(".thirdLeftPic img").src = "/assets/hand.jpg"; 
+    document.querySelector(".thirdLeftPic img").src = "./assets/hand.jpg"; 
 });
 document.querySelector(".disthree").addEventListener("click", () => {
-    document.querySelector(".thirdLeftPic img").src = "/assets/unique.jpg";  
+    document.querySelector(".thirdLeftPic img").src = "./assets/unique.jpg";  
 });
